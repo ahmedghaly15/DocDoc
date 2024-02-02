786408cf055d323981856c64d5719afc2a813e36
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-02-02 11:12:54 +0200

    Setup Dependency injection for login feature

--- lib/src/config/locator/setup_locator_for_external.dart
+++
@@ -1,8 +0,0 @@
-part of '../../../service_locator.dart';
-
-class SetupLocatorForExternal {
-  Future<void> setup() async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    getIt.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
-  }
-}
