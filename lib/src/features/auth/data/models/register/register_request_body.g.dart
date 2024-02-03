2c6587e1d6d9cc62cf8bee5ea9aaadbf950082c8
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-02-03 03:15:44 +0200

    Build RegisterRequestBody & RegisterResponse

---
+++ lib/src/features/auth/data/models/register/register_request_body.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'register_request_body.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+RegisterRequestBody _$RegisterRequestBodyFromJson(Map<String, dynamic> json) =>
+    RegisterRequestBody(
+      name: json['name'] as String,
+      email: json['email'] as String,
+      password: json['password'] as String,
+      phone: json['phone'] as String,
+      passwordConfirmation: json['password_confirmation'] as String,
+      gender: json['gender'] as String,
+    );
+
+Map<String, dynamic> _$RegisterRequestBodyToJson(
+        RegisterRequestBody instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'email': instance.email,
+      'password': instance.password,
+      'phone': instance.phone,
+      'password_confirmation': instance.passwordConfirmation,
+      'gender': instance.gender,
+    };
