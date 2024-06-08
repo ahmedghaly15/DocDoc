0e50f9d48e17a824b59293d2cb933c184d2b92fe
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-06-09 01:34:45 +0300

    Updated DioFactory

--- lib/src/core/api/dio_factory.dart
+++ lib/src/core/api/dio_factory.dart
@@ -15,11 +15,20 @@ class DioFactory {
       dio!
         ..options.connectTimeout = timeOut
         ..options.receiveTimeout = timeOut;
+      addDioHeaders();
       addDioInterceptor();
       return dio!;
     } else {
       return dio!;
     }
+  }
+
+  static void addDioHeaders() async {
+    dio?.options.headers = {
+      'Accept': 'application/json',
+      'Authorization':
+          'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3ZjYXJlLmludGVncmF0aW9uMjUuY29tL2FwaS9hdXRoL2xvZ2luIiwiaWF0IjoxNzE3ODYwMTA0LCJleHAiOjE3MTc4NjM3MDQsIm5iZiI6MTcxNzg2MDEwNCwianRpIjoiV2RxZVFsNGpDT0VCZ2tEVyIsInN1YiI6IjEyNDMiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.NpTZcXm-swOCpWLKP6Fnf3WVvtXBBwZNOI8VyMBiCWQ',
+    };
   }
 
   static void addDioInterceptor() {
